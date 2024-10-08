6fac8ad83ec8e46cdb51f6fa7f29b245f2d261cf
quang Pham <quangpham4089@gmail.com>
2024-10-08 18:59:20 +0700

    Your commit message

---
+++ Migrations/20241008101603_xedap.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebXeDapAPI.Migrations
+{
+    public partial class xedap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
