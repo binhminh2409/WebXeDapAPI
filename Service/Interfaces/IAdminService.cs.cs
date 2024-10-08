96afcddc0353e34034d66766155bab09f8dd9fc7
quang Pham <quangpham4089@gmail.com>
2024-10-08 19:27:07 +0700

    QuangYour commit message

---
+++ Service/Interfaces/IAdminService.cs.cs
@@ -0,0 +1,11 @@
+﻿using WebXeDapAPI.Dto;
+
+namespace WebXeDapAPI.Service.Interfaces
+{
+    public interface IAdminService
+    {
+        UserDto CreateEmployee(UserDto userDto); // Tạo tài khoản nhân viên
+        IEnumerable<UserDto> GetEmployees(); // Lấy danh sách nhân viên
+        bool DeleteEmployee(int employeeId); // Xóa nhân viên
+    }
+}
